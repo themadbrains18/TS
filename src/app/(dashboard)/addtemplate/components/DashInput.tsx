b3746c1bd977f3ccdb2fc67ac7bdbe17bc36add5
sunnymasih0253 <sunnyk.mdb@gmail.com>
2024-10-16 11:42:40 +0530

    changes in custom tab and input

--- src/app/(dashboard)/addtemplate/components/DashInput.tsx
+++ src/app/(dashboard)/addtemplate/components/DashInput.tsx
@@ -2,10 +2,10 @@ import { cn } from '@/libs/utils'
 import { dashinput } from '@/types/type'
 import React from 'react'
 
-const DashInput:React.FC<dashinput> = ({className,placeholder,type,value}) => {
+const DashInput:React.FC<dashinput> = ({className,placeholder,type,value, onChange}) => {
   return (
     <>
-    <input type={type} placeholder={placeholder} value={value} className={cn`border border-neutral-400 rounded-md placeholder:text-neutral-500 placeholder:capitalize capitalize w-full p-3 ${className}`} />
+    <input type={type} placeholder={placeholder} value={value} className={cn`border border-neutral-400 rounded-md placeholder:text-neutral-500 placeholder:capitalize capitalize w-full p-3 ${className}`} onChange={onChange}/>
     </>
   )
 }
