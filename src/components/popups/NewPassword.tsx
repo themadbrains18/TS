8df8ed601e74007b4268c79741f85ee836da14b9
sahil soni <sonisahil1018@gmail.com>
2024-11-06 18:39:27 +0530

    working on newpasword popup

--- src/components/popups/NewPassword.tsx
+++ src/components/popups/NewPassword.tsx
@@ -4,7 +4,7 @@ import React, { Fragment, useState } from 'react'
 import Modal from '../ui/Modal'
 import Button from '../ui/Button'
 import Icon from '../Icon';
-import CheckBox from '../ui/checkbox';
+import CheckBox from '../ui/Checkbox';
 import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import useFetch from '@/hooks/useFetch';
 import { zodResolver } from '@hookform/resolvers/zod';
