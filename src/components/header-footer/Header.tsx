bf2b8eea5098bbcc585508194056623715fb813c
Manish-taak <manish05.mdb@gmail.com>
2024-11-20 18:01:54 +0530

    changes

--- src/components/header-footer/Header.tsx
+++ src/components/header-footer/Header.tsx
@@ -390,7 +390,7 @@ const Header = () => {
                           title={`${item?.name}`}
                           titleboxclass="border-b"
                         >
-                          <NavTabs classname="max-w-[768px]:min-w-[148px]" setSidebar={() => setSidebar(!sidebar)} subCat={item?.subCategories} />
+                          <NavTabs  setSidebar={() => setSidebar(!sidebar)} subCat={item?.subCategories} />
                         </Accordion>
                       </Fragment>)
                     })
