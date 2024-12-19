ba64b0d48ad965451c498c9a81cddcde822b1a6f
sahil soni <sonisahil1018@gmail.com>
2024-12-19 16:12:06 +0530

    swiper update

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -48,7 +48,7 @@ import PurpleLoader from '@/../public/icons/purpleloader.svg'
 import Linevertical from '@/../public/icons/linevertical.svg'
 import Bouncingcircles from '@/../public/icons/bouncingcircles.svg'
 import Dashboard2 from '@/../public/icons/dashboard2.svg'
-import DownloadLogo from '@/../public/icons/downloadlogo.svg'
+import DownloadLogo from '@/../public/icons/downloadLogo.svg'
 import { cn } from "@/libs/utils";
 
 // Map icon names to components
